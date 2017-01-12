e28f20e9c8ebd178f812b21494dd23790868ce4d
ShuXian <test27c@gmail.com>
2017-01-12 10:59:40 +0000

    add full CRUD features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,12 +11,36 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
-      
     end
+  end
+
+  def index
+    @articles = Article.all
   end
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
   
   private
