efdcdb937c7067d9be7d48e9ba36d97861597c0a
ShuXian <test27c@gmail.com>
2017-01-17 02:59:10 +0000

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.all
+    # @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   
   def create
@@ -35,6 +36,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     
   end
   
