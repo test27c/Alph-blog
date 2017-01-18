5b0a8552813677ae5b48b3193566c048b09672b1
ShuXian <test27c@gmail.com>
2017-01-18 03:02:08 +0000

    Fix navigation using session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update, :show]
+  
   def new
     @user = User.new
   end
@@ -20,12 +22,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
-    
-  end
+      end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was successfully updated"
       redirect_to articles_path
@@ -35,15 +34,18 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     
   end
   
   private
   
-  def  user_params
+  def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :articles
   
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
 end
