f0eea8950dbd270925e3ca77eb0e69918cd22ec6
ShuXian <test27c@gmail.com>
2017-01-19 04:20:04 +0000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
   
   def new
     @article = Article.new
@@ -51,6 +53,13 @@ class ArticlesController < ApplicationController
   
     def article_params
       params.require(:article).permit(:title, :description )
+    end
+  
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "you can only edit or delete your own article!"
+        redirect_to root_path
+      end
     end
   
 end
