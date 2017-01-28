f46597a7074e8197ce3bdc722722c3c9a96eedd6
ShuXian <test27c@gmail.com>
2017-01-28 04:27:57 +0000

    more fix disqus confidential

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,5 +1,5 @@
 DisqusRails.setup do |config|
-  config::SHORT_NAME = "shu_xian"
+  config::SHORT_NAME = "alph-blog"
   config::SECRET_KEY = "" #leave blank if not used
   config::PUBLIC_KEY = "" #leave blank if not used
   config::ACCESS_TOKEN = "edc5e5ed916d4a109e900270323208ed" #you got it, right? ;-)
