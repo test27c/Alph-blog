3a904589e33f0886dca3e97d74f1782caabb90ed
ShuXian <test27c@gmail.com>
2017-01-28 03:56:39 +0000

    change disqus url

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,6 +1,6 @@
 DisqusRails.setup do |config|
   config::SHORT_NAME = "shu_xian"
-  config::SECRET_KEY = "IxX9YI0GneNDUXgEVJXrzHdI0sNQAw1cY0nhMhC9cxsGjeQpCo2qX6MkmmVO9yM0" #leave blank if not used
-  config::PUBLIC_KEY = "your XMVQPAHT2xAtv8qPHL4GHjWHboyiHj0xlk9HvMYu9Fv3a0QAhoFDxkcQRQyHs9p7" #leave blank if not used
-  config::ACCESS_TOKEN = "562aaa729a4e484295b17c8fa1fb22ac" #you got it, right? ;-)
+  config::SECRET_KEY = "k14a71DhSXiMQzTWXlRlWrYFZBqb9gmvpUgQKL8hgVhBxRAn3rUIGwd4e51D9sNf" #leave blank if not used
+  config::PUBLIC_KEY = "wHD3Hn4zrPBtJrdYlVOgyX4yAjtZCmKr1SeiSriA3DqiP3YsQFharoQYOJVdntCK" #leave blank if not used
+  config::ACCESS_TOKEN = "edc5e5ed916d4a109e900270323208ed" #you got it, right? ;-)
 end
