767be55603957c67064311e91d69d79b28bb7e5b
ShuXian <test27c@gmail.com>
2017-01-28 04:23:28 +0000

    more fix disqus

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,6 +1,6 @@
 DisqusRails.setup do |config|
   config::SHORT_NAME = "shu_xian"
-  config::SECRET_KEY = "k14a71DhSXiMQzTWXlRlWrYFZBqb9gmvpUgQKL8hgVhBxRAn3rUIGwd4e51D9sNf" #leave blank if not used
-  config::PUBLIC_KEY = "wHD3Hn4zrPBtJrdYlVOgyX4yAjtZCmKr1SeiSriA3DqiP3YsQFharoQYOJVdntCK" #leave blank if not used
+  config::SECRET_KEY = "" #leave blank if not used
+  config::PUBLIC_KEY = "" #leave blank if not used
   config::ACCESS_TOKEN = "edc5e5ed916d4a109e900270323208ed" #you got it, right? ;-)
 end
